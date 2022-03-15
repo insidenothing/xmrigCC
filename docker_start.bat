470c0417a6482743909fb92e87afc98ff79cc348
Patrick McGuire <insidenothing@gmail.com>
2022-03-15 16:35:17 -0400

    Update docker_start.bat

--- docker_start.bat
+++ docker_start.bat
@@ -1,3 +1,4 @@
 @echo off
-cd C:\Users\Patrick\Documents\xmrigCC_my
-docker run %1 -a cn/0 -o pool.bmorecoin.com:3333 -u bxcZxeUM9U63wkeTY6dXRgT97UDhyn3Ye9dLpGfKzgNCNNJCKA9Eq58SRWYhvSHsWzMZRrzNBycP38CTwxN79wGg2a52zRPGS -p x -k --cc-url=0.0.0.0:443 --cc-access-token=mySecret
+@echo off
+set /p id="Enter Container ID: "
+docker run %id% -a cn/0 -o pool.bmorecoin.com:3333 -u bxcZxeUM9U63wkeTY6dXRgT97UDhyn3Ye9dLpGfKzgNCNNJCKA9Eq58SRWYhvSHsWzMZRrzNBycP38CTwxN79wGg2a52zRPGS -p x -k --cc-url=0.0.0.0:443 --cc-access-token=mySecret
