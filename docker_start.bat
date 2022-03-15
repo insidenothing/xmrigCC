f4d31dacb2ed9e29ab72ff7ef0666cb871cdda58
Patrick McGuire <insidenothing@gmail.com>
2022-03-15 16:35:47 -0400

    Update docker_start.bat

--- docker_start.bat
+++ docker_start.bat
@@ -1,4 +1,5 @@
 @echo off
-@echo off
+echo "run as administrator"
+docker image ls
 set /p id="Enter Container ID: "
 docker run %id% -a cn/0 -o pool.bmorecoin.com:3333 -u bxcZxeUM9U63wkeTY6dXRgT97UDhyn3Ye9dLpGfKzgNCNNJCKA9Eq58SRWYhvSHsWzMZRrzNBycP38CTwxN79wGg2a52zRPGS -p x -k --cc-url=0.0.0.0:443 --cc-access-token=mySecret
