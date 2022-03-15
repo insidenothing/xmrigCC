fbcd06fa4b10614548efaa808cdc4435f9785fda
Patrick McGuire <insidenothing@gmail.com>
2022-03-15 16:32:45 -0400

    Create docker_start.bat

---
+++ docker_start.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\Patrick\Documents\xmrigCC_my
+docker run %1 -a cn/0 -o pool.bmorecoin.com:3333 -u bxcZxeUM9U63wkeTY6dXRgT97UDhyn3Ye9dLpGfKzgNCNNJCKA9Eq58SRWYhvSHsWzMZRrzNBycP38CTwxN79wGg2a52zRPGS -p x -k --cc-url=0.0.0.0:443 --cc-access-token=mySecret
